dc210f037fd8aca5206bca74d5574d7819fc3a9d
Phachara Kamthong <pcr.mixkungz@hotmail.com>
2018-10-13 13:39:15 +0700

    :sparkles: Add MaterialRepository.

---
+++ src/main/java/com/sit/cloudnative/MaterialService/MaterialRepository.java
@@ -0,0 +1,8 @@
+package com.sit.cloudnative.MaterialService;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialRepository extends JpaRepository<Material,String> {
+}
